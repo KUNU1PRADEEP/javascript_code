e7a75c029b0daf7bcbe8a29d516a26f1c7914cf9
Kunal Jaiswal <114349142+KUNU1PRADEEP@users.noreply.github.com>
2024-01-10 12:14:17 +0000

    for inbuilt fun

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -6,3 +6,30 @@ const values=coding.forEach((item)=>
         return item
     })
     console.log(values);
+
+    const myNums=[1,2,3,4,5,6,7,8,9,10]
+// call back
+//    const newNums= myNums.filter((num)=>num> 4)
+//    const newNums= myNums.filter((num)=>
+//     {
+//        return num>4 
+//     })
+
+const newNums=[]
+myNums.forEach(()=>{
+    if(num>4)
+    {
+        newNums.push(num)
+    }
+
+
+    })
+
+   console.log(newNums);
+
+
+// let userBooks=books.filter((bk)=>bk.genre==='History')
+// userBooks=books.filter((bk)=>{return bk.publish >=2000})
+//  userBooks=books.filter((bk)=>{return bk.publish >=2000 && bk.genre==="History"})
+
+// console.log(userBooks);
